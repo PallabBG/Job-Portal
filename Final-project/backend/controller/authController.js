52541ec772057dbc39419f58b98bfb2440185d04
PALLAB BANERJEE <pallabbanerjee074@gmail.com>
2026-07-30 17:03:30 +0530

    Update authController.js

--- Final-project/backend/controller/authController.js
+++ Final-project/backend/controller/authController.js
@@ -10,9 +10,7 @@ const geocoder = require("../server/utils/geocoder");
 
 const sendOtpMail = async (email, otp) => {
   const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 587,
-    secure: false, // STARTTLS
+    service: "gmail",
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
